abc5273493961549a38977dc4ee9183a5990b397
vardhman jain <jainvardhman766@gmail.com>
2025-01-25 12:25:05 +0530

    Your commit message

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,13 +4,14 @@ import "./HomePage.css";
 const Homepage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-950 to-blue-900 text-white homepage">
+     
       {/* Main Content */}
       <main className="max-w-4xl mx-auto mt-8 px-4 homepage-content">
         {/* Tagline */}
         <h2 className="text-2xl text-center mb-8 homepage-tagline">LET'S GROW TOGETHER</h2>
 
-        {/* Search Bar */}
-        <div className="relative max-w-2xl mx-auto mb-16 search-bar">
+{/* Search Bar */}
+<div className="relative max-w-2xl mx-auto mb-16 search-bar">
           <input
             type="text"
             className="w-full px-4 py-3 rounded-full bg-white/20 backdrop-blur-sm"
@@ -19,8 +20,8 @@ const Homepage = () => {
           <Search className="absolute right-4 top-3 text-gray-300 search-btn hover:text-white" />
         </div>
 
-        {/* Features Section */}
-        <div className="features">
+  {/* Features Section */}
+  <div className="features">
           {/* Tech News */}
           <a href="/technews" className="feature">
             <img src="./home/technews.png" alt="Tech News" />
@@ -50,22 +51,30 @@ const Homepage = () => {
             <img src="./home/resources.png" alt="Resources" />
             <p>Resources</p>
           </a>
+ {/* chatbot*/}
+          <a href="/chatbot " className="feature">
+            <img src="./home/chatbot.png" alt="Resources" />
+            <p>Chatbot</p>
+          </a>
+          
         </div>
 
-        <div className="social-links-container">
-          <h3 className="social-links-heading">Follow us on:</h3>
-          <div className="social-icons">
-            <a href="https://facebook.com" className="social-link" target="_blank">
-              <img src="./home/facebook.png" alt="Facebook" className="social-icon" />
-            </a>
-            <a href="https://twitter.com" className="social-link" target="_blank">
-              <img src="./home/twitter.png" alt="Twitter" className="social-icon" />
-            </a>
-            <a href="https://instagram.com" className="social-link" target="_blank">
-              <img src="./home/instagram.png" alt="Instagram" className="social-icon" />
-            </a>
-          </div>
-        </div>
+        <div class="social-links-container">
+    <h3 class="social-links-heading">Follow us on:</h3>
+    <div class="social-icons">
+        <a href="https://facebook.com" class="social-link" target="_blank">
+            <img src="./home/facebook.png" alt="Facebook" class="social-icon" />
+        </a>
+        <a href="https://twitter.com" class="social-link" target="_blank">
+            <img src="./home/twitter.png" alt="Twitter" class="social-icon" />
+        </a>
+        <a href="https://instagram.com" class="social-link" target="_blank">
+            <img src="./home/instagram.png" alt="Instagram" class="social-icon" />
+        </a>
+    </div>
+</div> 
+
+         
       </main>
     </div>
   );
